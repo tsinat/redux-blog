0ed67e2c95178acda2b702f2424d3fc4bdec2513
Tsinat Zeree <cheru87@gmail.com>
2016-11-06 23:24:34 -0800

    todo action creators

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+const ADD_TODO = 'ADD_TODO';
+const REMOVE_TODO = 'REMOVE_TODO';
+
+export function addTodo(item) {
+    return {
+        type: ADD_TODO,
+        payload: item
+    }
+}
+
+export function removeTodo(id) {
+    return {
+        type: REMOVE_TODO,
+        payload: id
+    }
+}
