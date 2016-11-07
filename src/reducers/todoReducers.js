d403853eab770e23871cbdbb571a9a7c1ec36c0e
Tsinat Zeree <cheru87@gmail.com>
2016-11-07 00:11:52 -0800

    todo reducers

---
+++ src/reducers/todoReducers.js
@@ -0,0 +1,26 @@
+// src/reducers/todoReducers.js
+
+import shortid from 'shortid';
+
+export default function( state = [], action) {
+    switch(action.type) {
+        case ADD_TODO:
+            return [
+                ...state,
+                {
+                    id: shortid.generate(),
+                    text: action.payload
+                }
+             ];
+        case REMOVE_TODO:
+            const index = state.indexOf(action.payload);
+            if(index >= 0) {
+                return [
+                    ...state.slice(0, index),
+                    ...state.slice(index+1)
+                ];
+            }
+        default: return state;
+    }
+    return state;
+}
