888cdf85d928b3291a16ef08d0d4cf74ca634f86
Tsinat Zeree <cheru87@gmail.com>
2016-11-07 02:52:25 -0800

    add todo form

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,46 @@
 import React, { Component } from 'react';
 
-export default class App extends Component {
+class App extends Component {
+    constructor(props){
+        super(props)
+
+        this.displayTodo = this.displayTodo.bind(this);
+        this.state = { term: "" };
+    }
+    handleChange(e) {
+        e.preventDefault();
+        this.setState({ term: e.target.value})
+    }
+    addTodo(e) {
+        e.preventDefault();
+        //dispatch action
+    }
+    displayTodo(){
+        console.log('it is working');
+    }
   render() {
     return (
-      <div>React simple starter</div>
+        <div className="row">
+            <div className="col-sm-6 col-sm-offset-3">
+                <form className="form-inline" onSubmit={ this.addTodo.bind(this)}>
+                    <div className="form-group">
+                        <input
+                            className="form-control"
+                            type="text"
+                            value={this.state.term}
+                            placeholder="add todo"
+                            onChange={ this.handleChange.bind(this)}
+                            />
+                        <button className="btn btn-default">Add</button>
+                    </div>
+                </form>
+
+                { this.displayTodo()}
+
+            </div>
+        </div>
     );
   }
 }
+
+export default App;
